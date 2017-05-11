768f25a1872b4d37582e5ae79e679ef74241fd19
VaniaStoicheva <cecostoi4ev@abv.bg>
2017-05-11 14:14:48 +0300

    profile

--- src/BookShopBundle/Entity/User.php
+++ src/BookShopBundle/Entity/User.php
@@ -22,7 +22,7 @@ class User implements UserInterface
 
     const ROLE_USER = 'ROLE_USER';
     const ROLE_ADMIN= 'ROLE_ADMIN';
-    const ROLE_SUPER_ADMIN=  'ROLE_SADMIN';
+    const ROLE_EDITOR=  'ROLE_EDITOR';
 
     /**
      * @var int
