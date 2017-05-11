c15877e2d42710c04240acddfb5cc560c489b206
VaniaStoicheva <cecostoi4ev@abv.bg>
2017-05-11 13:03:08 +0300

    add profile

---
+++ src/BookShopBundle/Transformers/PromotionTransformer.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1234
+ * Date: 10.5.2017 г.
+ * Time: 16:44 ч.
+ */
+
+namespace BookShopBundle\Transformers;
+
+
+use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\Form\Exception\TransformationFailedException;
+
+class PromotionTransformer implements DataTransformerInterface
+{
+
+
+    public function transform($value)
+    {
+        // TODO: Implement transform() method.
+    }
+
+
+    public function reverseTransform($value)
+    {
+        // TODO: Implement reverseTransform() method.
+    }
+}
